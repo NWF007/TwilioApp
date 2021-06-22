52f4fdbe0187b29d8728678b07689fe7e709caee
Nico <ncfortuin27@gmail.com>
2021-06-22 16:45:55 +0200

    first commit

---
+++ TwilioApp/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using TwilioApp.Models;
+
+namespace TwilioApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Create(string number, string message)
+        {
+            if (ModelState.IsValid)
+            {
+                string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+                string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+                TwilioClient.Init(accountSid, authToken);
+
+                var sendmessage = MessageResource.Create(
+                    body: message,
+                    from: new Twilio.Types.PhoneNumber("+15005550006"),
+                    to: new Twilio.Types.PhoneNumber(number)
+                );
+
+                Console.WriteLine(sendmessage.Sid);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
